8d9890974f2b81df235ad7fe4377a931e3fa3c2d
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2024-11-19 22:04:06 +0000

    Add reports

--- src/main.py
+++ src/main.py
@@ -15,14 +15,14 @@ def main():
 
     # Extract existing data from EPD
     date_from = "earliest"  # Can be "YYYYMM" or "earliest" or "latest", default="earliest"
-    date_to = "202402"  # Can be "YYYYMM" or "latest" or "latest-1", default="latest"
+    date_to = "latest-1"  # Can be "YYYYMM" or "latest" or "latest-1", default="latest"
 
     # Fetch existing data using BSA API
     existing_data_extract = bsa_utils.FetchData(resource=dataset_id, date_from=date_from, date_to=date_to, sql=sql, cache=True)
 
     # Extract latest data from EPD
-    date_from = "202403"  # Can be "YYYYMM" or "earliest" or "latest", default="earliest"
-    date_to = date_from  # Can be "YYYYMM" or "latest" or "latest-1", default="latest"
+    date_from = "latest"  # Can be "YYYYMM" or "earliest" or "latest", default="earliest"
+    date_to = "latest"  # Can be "YYYYMM" or "latest" or "latest-1", default="latest"
 
     # Fetch latest data using BSA API
     latest_data_extract = bsa_utils.FetchData(resource=dataset_id, date_from=date_from, date_to=date_to, sql=sql)
